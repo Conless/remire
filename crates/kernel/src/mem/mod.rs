836775d50690099aa316ca00ec3e4e9490150279
Yi Pan <conlesspan@outlook.com>
2024-04-10 01:26:39 +0800

    feat(kernel): allocator is thread-safe now.

--- crates/kernel/src/mem/mod.rs
+++ crates/kernel/src/mem/mod.rs
@@ -1,45 +1,46 @@
-use core::alloc::GlobalAlloc;
+// Copyright (c) 2024 Conless Pan
 
-use crate::sync::UPSafeCell;
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+use core::{alloc::{GlobalAlloc, Layout}, borrow::BorrowMut};
+
+use spin::Mutex;
+
+use crate::config::KERNEL_HEAP_SIZE;
 
 use self::buddy::BuddyAllocator;
 
 mod buddy;
 mod avl;
 
-const KERNEL_HEAP_SIZE: usize = 0x1000000;
-
 static mut KERNEL_HEAP_SPACE: [u8; KERNEL_HEAP_SIZE] = [0; KERNEL_HEAP_SIZE];
 
-pub struct UpBuddyAllocator {
-    inner: UPSafeCell<BuddyAllocator>,
-}
+pub struct LockedAllocator(Mutex<BuddyAllocator>);
 
-impl UpBuddyAllocator {
+impl LockedAllocator {
     pub const fn empty() -> Self {
-        Self {
-            inner: unsafe { UPSafeCell::new(BuddyAllocator::empty()) },
-        }
+        LockedAllocator(Mutex::new(BuddyAllocator::empty()))
     }
 
     pub fn init(&self) {
         unsafe {
             let start = KERNEL_HEAP_SPACE.as_ptr() as usize;
             let end = start + KERNEL_HEAP_SPACE.len();
-            self.inner.borrow_mut().add_segment(start, end);
+            self.0.lock().borrow_mut().add_segment(start, end);
         }
     }
 }
 
-unsafe impl GlobalAlloc for UpBuddyAllocator {
-    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
-        self.inner.borrow_mut().alloc(layout)
+unsafe impl GlobalAlloc for LockedAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        self.0.lock().borrow_mut().alloc(layout)
     }
 
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
-        self.inner.borrow_mut().dealloc(ptr, layout);
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        self.0.lock().borrow_mut().dealloc(ptr, layout);
     }
 }
 
 #[global_allocator]
-pub static HEAP_ALLOCATOR: UpBuddyAllocator = UpBuddyAllocator::empty();
+pub static HEAP_ALLOCATOR: LockedAllocator = LockedAllocator::empty();
