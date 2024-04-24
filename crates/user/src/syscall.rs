7a1b2d457c8065a0d6c6ec44dcd5e2ae1ef48b3f
Conless <conlesspan@outlook.com>
2024-04-25 00:12:16 +0800

    fix(user): linking elf instead of raw file.

--- crates/user/src/syscall.rs
+++ crates/user/src/syscall.rs
@@ -9,6 +9,7 @@ const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_YIELD: usize = 124;
 const SYSCALL_GET_TIME: usize = 169;
+const SYSCALL_SBRK: usize = 214;
 
 /// System call
 /// 
@@ -48,3 +49,8 @@ pub struct TimeVal {
 pub fn sys_get_time() -> isize {
     syscall(SYSCALL_GET_TIME, [0, 0, 0])
 }
+
+pub fn sys_sbrk(size: i32) -> isize {
+    syscall(SYSCALL_SBRK, [size as usize, 0, 0])
+}
+
