3356930d5fedfcc0f6e169296acbf3dca2852087
Conless <conlesspan@outlook.com>
2024-05-01 18:02:08 +0800

    feat(kernel): add pid allocator and new kernel stack.

--- crates/kernel/src/stack.rs
+++ crates/kernel/src/stack.rs
@@ -3,47 +3,41 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
+use crate::mm::{MapPermission, KERNEL_SPACE};
 use crate::{config::*, trap::TrapContext};
+use crate::task::pid::PIDGuard;
 
-#[repr(align(4096))]
-#[derive(Clone, Copy)]
+#[derive(Default, Debug)]
 pub struct KernelStack {
-    data: [u8; KERNEL_STACK_SIZE],
+    top: usize,
+    bottom: usize,
 }
 
-#[repr(align(4096))]
-#[derive(Clone, Copy)]
-pub struct UserStack {
-    data: [u8; USER_STACK_SIZE],
+fn get_kernel_stack_addr(pid: usize) -> (usize, usize) {
+    let top = TRAMPOLINE - pid * (KERNEL_STACK_SIZE + PAGE_SIZE);
+    let bottom = top - KERNEL_STACK_SIZE;
+    (top, bottom)
 }
-
-pub static KERNEL_STACK: [KernelStack; APP_MAX_NUM] = [KernelStack {
-    data: [0; KERNEL_STACK_SIZE],
-}; APP_MAX_NUM];
-
-pub static USER_STACK: [UserStack; APP_MAX_NUM] = [UserStack {
-    data: [0; USER_STACK_SIZE],
-}; APP_MAX_NUM];
 
 impl KernelStack {
-    /// Get the stack pointer of the kernel stack
-    fn get_sp(&self) -> usize {
-        self.data.as_ptr() as usize + KERNEL_STACK_SIZE
+    pub fn new(pid: &PIDGuard) -> Self {
+        let pid = pid.0;
+        let (top, bottom) = get_kernel_stack_addr(pid);
+        KERNEL_SPACE.borrow_mut().insert(
+            bottom.into(),
+            top.into(),
+            MapPermission::R | MapPermission::W,
+        );
+        KernelStack { top, bottom }
     }
-
-    /// Push the context to the kernel stack
-    pub fn push_context(&self, ctx: TrapContext) -> usize {
-        let ctx_ptr = (self.get_sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
-        unsafe {
-            *ctx_ptr = ctx;
-        }
-        ctx_ptr as usize
+    
+    pub fn get_top(&self) -> usize {
+        self.top
     }
 }
 
-impl UserStack {
-    /// Get the stack pointer of the user stack
-    pub fn get_sp(&self) -> usize {
-        self.data.as_ptr() as usize + USER_STACK_SIZE
+impl Drop for KernelStack {
+    fn drop(&mut self) {
+        KERNEL_SPACE.borrow_mut().remove(self.bottom.into());
     }
 }
---
+++ crates/kernel/src/task/pid.rs
@@ -0,0 +1,36 @@
+// Copyright (c) 2024 Conless Pan
+
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+use alloc::vec::Vec;
+use allocator::StackAllocator;
+use lazy_static::lazy_static;
+
+use crate::sync::UPSafeCell;
+
+lazy_static! {
+    pub static ref PID_ALLOCATOR: UPSafeCell<StackAllocator> = 
+      unsafe { UPSafeCell::new(StackAllocator::default()) };
+}
+
+pub fn init_pid_allocator(min: usize, max: usize) {
+    PID_ALLOCATOR.borrow_mut().init(min, max);
+}
+
+pub struct PIDGuard(pub usize);
+
+impl Drop for PIDGuard {
+  fn drop(&mut self) {
+      //println!("drop pid {}", self.0);
+      PID_ALLOCATOR.borrow_mut().dealloc(self.0);
+  }
+}
+
+pub fn alloc_pid() -> Option<PIDGuard> {
+  let pid = PID_ALLOCATOR.borrow_mut().alloc();
+  // println!("alloc pid {}", pid);
+  pid.map(PIDGuard)
+}
+
+
