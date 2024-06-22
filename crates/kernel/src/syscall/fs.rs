d012e5ff2ed0b3a87beaeb8611fb83888bc02cee
Yi Pan <conlesspan@outlook.com>
2024-06-23 02:10:06 +0800

    fix(kernel): minor adjustments in kernel.

--- crates/kernel/src/syscall/fs.rs
+++ crates/kernel/src/syscall/fs.rs
@@ -28,18 +28,15 @@ pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
 pub fn sys_read(fd: usize, buf: *mut u8, len: usize) -> isize {
     match fd {
         FD_STDIN => {
-            assert_eq!(len, 1, "Only support len = 1 in sys_read!");
-            let mut c: usize;
-            loop {
-                c = console_getchar();
-                if c == 0 {
+            let ch = loop {
+                let input = console_getchar();
+                if input == 0 {
                     suspend_current_and_run_next();
                     continue;
                 } else {
-                    break;
+                    break input;
                 }
-            }
-            let ch = c as u8;
+            } as u8;
             let mut buffers = translated_byte_buffer(current_user_token(), buf, len);
             unsafe { buffers[0].as_mut_ptr().write_volatile(ch); }
             1
